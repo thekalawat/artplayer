07442e2fd229827a78e008b7a7b17fea1450d505
thekalawat <75123123+thekalawat@users.noreply.github.com>
2025-06-05 12:58:29 +0530

    Add files via upload

---
+++ artplayer.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: ArtPlayer
+Description: Adds a shortcode to embed ArtPlayer video player.
+Version: 1.0.0
+*/
+
+defined('ABSPATH') or die('Not Authorized!');
+
+include('includes/main.php');
